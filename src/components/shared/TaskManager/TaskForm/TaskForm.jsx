5c4acf4876950a06accf4abeecb471d5f68d778d
sthakkar <sthakkar@benunets.com>
2021-05-01 23:32:22 -0400

    State management

--- src/components/shared/TaskManager/TaskForm/TaskForm.jsx
+++ src/components/shared/TaskManager/TaskForm/TaskForm.jsx
@@ -2,60 +2,71 @@ import React, {Component} from 'react'
 import {Form, FormGroup, Input, Label, Button} from 'reactstrap';
 
 class TaskForm extends Component{
-
-    titleErr = ''
-    descErr = ''
+    state = {
+        title : '',
+        description: '',
+        titleErr : '',
+        descErr : ''
+    }
 
     handleSubmit = (e) => {
         e.preventDefault()
-        const title = document.getElementById('title');
-        const description = document.getElementById('description');
+        const {title, description} = this.state
 
         let valid = true;
 
-        if(title.value === ''){
-            this.titleErr = 'Title cannot be blank'
-            this.forceUpdate()
+        if(title === ''){
+            this.setState({
+                titleErr: 'Title can not be blank.'
+            })
             valid = false 
         }
+        else{
+            this.setState({
+                titleErr: ''
+            })
+        }
 
-        if(description.value === ''){
-            this.descErr = 'Description cannot be blank'
-            this.forceUpdate()
+        if(description === ''){
+            this.setState({
+                descErr : 'Desctiption can not be blanked'
+            })
             valid = false 
         }
 
         if(valid){
             const data = {
-                title: title.value,
-                description: description.value,
+                title,
+                description
             }
+
             this.props.addTask(data)
 
-            title.value = ''
-            description.value = ''
-            this.titleErr = ''
-            this.descErr = ''
-            this.forceUpdate()
+            this.setState({
+                title : '',
+                description : '',
+                titleErr : '',
+                descErr : ''
+            })
         }
     }
     
     render(){
-
+        const {title, description, titleErr, descErr} = this.state;
         return (
             <Form onSubmit={this.handleSubmit}>
             <FormGroup>
                 <Label htmlFor='title'>Title</Label>
-                <Input id='title'></Input>
+                <Input id='title' value={title} onChange = {(e) => this.setState({title: e.target.value})}></Input>
                 <span id="titleErr" style={{color: 'red', fontSize: '10px'}}>
-                    {this.titleErr}
+                    {titleErr}
                 </span>
             </FormGroup>
             <FormGroup>
                 <Label htmlFor='description'>Description</Label>
-                <Input type="textarea" id='description' rows='5'></Input>
+                <Input type="textarea" value={description} id='description' rows='5' onChange={(e)=>{this.setState({description : e.target.value})}}></Input>
                 <span id="descErr" style={{color: 'red', fontSize: '10px'}}>
-                    {this.descErr}
+                    {descErr}
                 </span>
             </FormGroup>
 
