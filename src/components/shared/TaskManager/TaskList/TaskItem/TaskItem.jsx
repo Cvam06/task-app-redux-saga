d13a481ec5df9b2717579992aa3cc96993ad2adc
sthakkar <sthakkar@benunets.com>
2021-05-19 16:36:00 -0400

    Added logger with deleteTask functionality

--- src/components/shared/TaskManager/TaskList/TaskItem/TaskItem.jsx
+++ src/components/shared/TaskManager/TaskList/TaskItem/TaskItem.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import {ListGroupItem, Button} from 'reactstrap';
+import {connect} from 'react-redux';
+import {deleteTaskAction} from "../../../../../actions/taskActions.js";
 
-function TaskItem({key, task}) {
+function TaskItem({key, task, deleteTask}) {
+    const handleDelete = () => {
+        deleteTask(task.id)
+    }
     return (
         <ListGroupItem>
             {task.title}
-            <Button close></Button>
+            <Button close onClick={handleDelete}></Button>
         </ListGroupItem>
     )
 }
 
-export default TaskItem
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteTask: (id) => {
+            dispatch(deleteTaskAction(id))
+        }
+    }
+}
+export default connect(null, mapDispatchToProps)(TaskItem)
--- src/reducer/rootReducer.js
+++ src/reducer/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import { taskReducer } from './taskReducer';
+// import { userReducer } from './userReducer';
 
 const rootReducer = combineReducers({
-    tasks: taskReducer
+    tasks: taskReducer,
+    user: () => null,
 })
 
 export default rootReducer;
